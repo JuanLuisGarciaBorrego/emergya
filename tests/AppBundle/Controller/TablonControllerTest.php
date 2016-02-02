f2a2c290884c3e1bd570e62a0f03214ce984c7b7
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-02-02 14:03:46 +0100

    test homepage status 200

--- tests/AppBundle/Controller/TablonControllerTest.php
+++ tests/AppBundle/Controller/TablonControllerTest.php
@@ -4,15 +4,14 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class TablonControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Tablón Emergya', $crawler->filter('h1')->text());
     }
 }
