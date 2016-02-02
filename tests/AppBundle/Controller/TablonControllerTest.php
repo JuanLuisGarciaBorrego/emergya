18103b5b9b28f0a75116850174974396d8c8a6e3
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-02-02 14:10:28 +0100

    test publised

--- tests/AppBundle/Controller/TablonControllerTest.php
+++ tests/AppBundle/Controller/TablonControllerTest.php
@@ -14,4 +14,21 @@ class TablonControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Tablón Emergya', $crawler->filter('h1')->text());
     }
+
+    public function testNewMessage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/crear-mensaje');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $buttonCrawler = $crawler->selectButton('Enviar mensaje')->form();
+
+        $buttonCrawler['user[nick]'] = rand(0, 100)."usernick";
+        $buttonCrawler['user[message]'] = 'Descripción de prueba';
+
+        $client->submit($buttonCrawler);
+        $this->assertEquals(200, $client->getResponse()->isRedirect());
+        $client->followRedirect();
+    }
 }
